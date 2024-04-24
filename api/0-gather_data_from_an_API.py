1e03c5a791069aaf2bbaf61e8c1303e5fe6c6551
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-04-24 17:32:27 +0400

    pep fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python3
-import requests
+"""
+This module retrieves task information from an API based on a user ID
+anddisplays the employee's name,
+the number of tasks completed, and the titles of the completed tasks.
+"""
 
+import requests
 import sys
 
-TASK_URL = 'https://jsonplaceholder.typicode.com/todos'
-USER_URL = 'https://jsonplaceholder.typicode.com/users/'
+TASK_URL = "https://jsonplaceholder.typicode.com/todos"
+USER_URL = "https://jsonplaceholder.typicode.com/users/"
 
 
 def employee_info(user_id: str):
+    """
+    Retrieves task information from an API based
+    on a user ID and returns a formatted string
+    containing the employee's name, the number of tasks completed, and the
+    titles of the completed tasks.
+
+    Args:
+        user_id (str): The ID of the user.
+
+    Returns:
+        str: A formatted string containing the
+        employee's name, the number of tasks completed,
+        and the titles of the completed tasks.
+    """
     task_response = requests.get(TASK_URL + "?userId=" + user_id).json()
     user_response = requests.get(USER_URL + user_id).json()
 
-    name = user_response.get('name')
+    name = user_response.get("name")
     total_tasks = len(task_response)
-    done_tasks = len([task for task in task_response if task.get('completed') is True])
-    title_tasks = [task.get('title') for task in task_response if task.get('completed') is True]
-    
-    result = f"Employee {name} is done with tasks({done_tasks}/{total_tasks}):\n\t "
+    done_tasks = len([task for task in task_response if
+                      task.get("completed") is True])
+    title_tasks = [
+        task.get("title") for task in task_response if
+        task.get("completed") is True
+    ]
+
+    result = "Employee {} is done withtasks({}/{}):\n\t ".format(name,
+                                                                 done_tasks,
+                                                                 total_tasks)
     result += "\n\t ".join(title_tasks)
 
     return result

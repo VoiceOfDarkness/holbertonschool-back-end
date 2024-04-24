b0d78e9836107cf728aa9c9fc24ae155bfef2003
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-04-24 21:21:11 +0400

    hot fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,47 +1,32 @@
 #!/usr/bin/python3
 """
-This module retrieves task information from an API based on a user ID
-anddisplays the employee's name,
-the number of tasks completed, and the titles of the completed tasks.
+Requesting data from rest api
 """
-
 import csv
 import requests
 import sys
 
-TASK_URL = "https://jsonplaceholder.typicode.com/todos"
-USER_URL = "https://jsonplaceholder.typicode.com/users/"
-
-
-def employee_info(user_id: str):
-    """
-    Retrieves task information from an API based
-    on a user ID and returns a formatted string
-    containing the employee's name, the number of tasks completed, and the
-    titles of the completed tasks.
-
-    Args:
-        user_id (str): The ID of the user.
-
-    Returns:
-        str: A formatted string containing the
-        employee's name, the number of tasks completed,
-        and the titles of the completed tasks.
-    """
-    task_response = requests.get(TASK_URL + "?userId=" + user_id).json()
-    user_response = requests.get(USER_URL + user_id).json()
-
-    name = user_response.get("username")
-
-    with open(f"{user_id}.csv", "w") as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for task in task_response:
-            writer.writerow([user_id, name, task.get("completed"),
-                             task.get("title")])
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        raise ValueError("Usage: 1-export_to_CSV.py <employee_id>")
-    user_id = sys.argv[1]
-    employee_info(user_id)
+    if (len(sys.argv) < 2):
+        print("Please enter user id")
+        sys.exit()
+
+    link = 'https://jsonplaceholder.typicode.com'
+
+    user = requests.get('{0}/users/{1}'
+                        .format(link, sys.argv[1])).json()
+    userTodo = requests.get('{0}/todos?{1}={2}'
+                            .format(link, "userId", sys.argv[1])).json()
+
+    csvFormat = []
+
+    for data in userTodo:
+        arr = [f"{data['userId']}", f"{user['username']}",
+               f"{data['completed']}", f"{data['title']}"]
+        csvFormat.append(arr)
+
+    filename = f"{user['id']}.csv"
+
+    with open(filename, 'w', newline="") as file:
+        csvwriter = csv.writer(file, quoting=csv.QUOTE_ALL)
+        csvwriter.writerows(csvFormat)
